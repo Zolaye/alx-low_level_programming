4215b03bf63332106c320f4a60546cf6e7c22b1a
Zolaye <mzele8030@gmail.com>
2022-03-11 02:55:52 +0300

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main entry point
+ * return return 1 after program excution
+ * /
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,"\n);
+return (0);
+}
