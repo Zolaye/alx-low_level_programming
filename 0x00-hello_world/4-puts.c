e126d7b9eee3612317b0de43c348af3caade100c
Zolaye <mzele8030@gmail.com>
2022-03-11 02:17:47 +0300

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+[A[C[C[C[C[C[C[C[A[B#include<>[Dioststdio.h[3~[3~[3~[C>[D[D[D[D[D[D[D[D[D [B[D[D[D[D[D[D[D[D[D/**
+* main entry point
+*return 0 after printing function
+*/
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+retorn (0);
+}
